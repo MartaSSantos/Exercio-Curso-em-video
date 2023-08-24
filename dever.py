7318bd68b43cdbc1b38585a3b0f207bdf7693594
marta-santos <martasilvaa325@gmail.com>
2023-08-24 10:38:06 -0300

    dever resolvido

---
+++ dever.py
@@ -0,0 +1,14 @@
+# desafio
+
+
+nome = input('Informe seu nome por gentileza. ' )
+print('Olá', nome ,' é muito bom te ter aqui! ')
+
+
+# desafio 2
+
+dia = input('Informe o dia que voce nesceu:')
+mes = input('Informe o mes que voce nesceu:')
+ano = input('Informe o ano que voce nesceu:')
+
+print('Voce nasceu em' , dia,  'de', mes, 'do ano de ' , ano) 
