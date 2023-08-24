61f9bd929001263b0626d372204d912d582ccabb
marta-santos <martasilvaa325@gmail.com>
2023-08-24 11:24:11 -0300

    comentando o codigo

--- dever.py
+++ dever.py
@@ -1,4 +1,4 @@
-# desafio
+# desafio 1
 
 
 nome = input('Informe seu nome por gentileza. ' )
@@ -6,6 +6,8 @@ print('Olá', nome ,' é muito bom te ter aqui! ')
 
 
 # desafio 2
+
+#neste desafio foi pedido para fazermos um programinha que perguntase o dia do nascimento, mes e ano. E ao final o progrma da uma resposta.
 
 dia = input('Informe o dia que voce nesceu:')
 mes = input('Informe o mes que voce nesceu:')
